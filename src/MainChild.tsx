c3d7340b0d08e17f9c2eb2ad6fdf7a2e0eb9f090
dineshpatel21 <92016407+dineshpatel21@users.noreply.github.com>
2025-07-08 18:57:43 +0530

    main child added.

---
+++ src/MainChild.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MainChild = () => {
+  return (
+    <View>
+      <Text>MainChild</Text>
+    </View>
+  )
+}
+
+export default MainChild
+
+const styles = StyleSheet.create({})
