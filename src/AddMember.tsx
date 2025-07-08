99df4ea62ffe61f7fb56252a80bb67367ee91622
Dinesh Patel <92016407+dineshpatel21@users.noreply.github.com>
2025-07-08 18:52:28 +0530

    Branch1 (#2)
    * third commit.
    
    * forth commit.

--- src/AddMember.tsx
+++ src/AddMember.tsx
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const AddMember = () => {
   return (
     <View>
-      <Text>AddMember</Text>
+      <TouchableOpacity>
+         <Text>Jalwa</Text>
+      </TouchableOpacity>
     </View>
   )
 }
