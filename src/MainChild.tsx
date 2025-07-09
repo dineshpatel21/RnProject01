17424a6ab04ad4862b6d62b9281c099317b71fe8
dineshpatel21 <92016407+dineshpatel21@users.noreply.github.com>
2025-07-09 17:53:16 +0530

    jldfjglds

--- src/MainChild.tsx
+++ src/MainChild.tsx
@@ -5,6 +5,7 @@ const MainChild = () => {
   return (
     <View>
       <Text>MainChild</Text>
+      <Text>i jj skks djfdlk sljdl sldjl sljflj sljfljs sljdfljl sljljld sldjljls lsdjflkjl sldfjljdl sldjfljd</Text>
     </View>
   )
 }
