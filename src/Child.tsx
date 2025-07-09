2a8f0805886aba499edfdf2e9e3aaf94b2a4db0a
dineshpatel21 <92016407+dineshpatel21@users.noreply.github.com>
2025-07-09 15:01:57 +0530

    adas

--- src/Child.tsx
+++ src/Child.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Child = () => {
   const onPressChild = ()=>{
-    console.log("Main Child pressed on Main Page !");
+    console.log("Main Child pressed on Main Page ! ljdfls lsjdlfsj lsdjfkljsdk d");
   }
   return (
     <View>
