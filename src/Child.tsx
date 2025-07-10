81e6ed6dfbe5c85078f0a239bb222c0ab14f445f
dineshpatel21 <92016407+dineshpatel21@users.noreply.github.com>
2025-07-10 15:22:21 +0530

    branch name renamed

--- src/Child.tsx
+++ src/Child.tsx
@@ -9,14 +9,7 @@ const Child = () => {
     <View>
       <TouchableOpacity onPress={()=>{onPressChild()}}>Child</TouchableOpacity>
       <View>
-        <Text>Hello</Text>
-        <TouchableOpacity><Text>Clicked!</Text></TouchableOpacity>
-        <Text>Today</Text>
-        <Text>Tommarow</Text>
-        <Text>Mangel Pandey</Text>
-        <Text>hello i am dinesh patel and i react native developer. asldkjklsa  alsdjlkfk alkdsjkl </Text>
-        <Text>yes your name is what?</Text>
-        <Text>hello this is india.</Text>
+        
       </View>
     </View>
   )
